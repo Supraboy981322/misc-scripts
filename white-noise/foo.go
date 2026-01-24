2f1f19fcdcd897046c67d7cf4e278dbd39936690
keeper of the interwebs <97927259+Supraboy981322@users.noreply.github.com>
2026-01-24 11:28:58 -0600

    cleanup

--- white-noise/foo.go
+++ white-noise/foo.go
@@ -11,13 +11,16 @@ import (
 )
 
 func main() {
+	//gernerate a buffer
 	buf_f := gen_noise(10000000, 1.0)
 	buf_b := f_to_b_buf(buf_f)
 
+	//ffplay
 	playback := exec.Command(
 		"ffplay",
 			"-f", "wav",
 			"-i", "-")
+	//ffmpeg
 	audio := exec.Command(
 		"ffmpeg",
       "-f", "f32le",
@@ -28,24 +31,29 @@ func main() {
 			"-f", "wav", 
 			"pipe:")
 
-	r, w := io.Pipe()
+	//connect audio and playback pipes to each other and terminal
+	r, w := io.Pipe()//reader and writer 
 	audio.Stdin, audio.Stdout, audio.Stderr = buf_b, w, os.Stderr
 	playback.Stdin, playback.Stderr = r, os.Stderr
-	
-	if err1, err2 := audio.Start(), playback.Start(); err1 != nil || err2 != nil {
-		fmt.Fprintf(
-			os.Stderr,
-			"failed to start:\n\taudio: %v\n\tplayback: %v\n",
-			err1, err2)
-		os.Exit(1)
-	}
 
-	if err1, err2 := audio.Wait(), playback.Wait(); err1 != nil || err2 != nil {
-		fmt.Fprintf(
-			os.Stderr,
-			"Failed to run:\n\taudio: %v\n\tplayback: %v\n",
-			err1, err2)
-		os.Exit(1)
+	{ //run containerization and playback 
+		err1, err2 := audio.Start(), playback.Start()
+		if err1 != nil || err2 != nil {
+			fmt.Fprintf(
+				os.Stderr,
+				"failed to start:\n\taudio: %v\n\tplayback: %v\n",
+				err1, err2)
+			os.Exit(1)
+		}
+	};{ //wait for cmds to finish
+		err1, err2 := audio.Wait(), playback.Wait()
+		if err1 != nil || err2 != nil {
+			fmt.Fprintf(
+				os.Stderr,
+				"Failed to run:\n\taudio: %v\n\tplayback: %v\n",
+				err1, err2)
+			os.Exit(1)
+		}
 	}
 }
 

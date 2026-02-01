4f6b5e94deddfb61a37a03b70c58962b834ae56f
keeper of the interwebs <97927259+Supraboy981322@users.noreply.github.com>
2026-01-31 21:43:36 -0600

    more opts

--- nS/main.go
+++ nS/main.go
@@ -9,8 +9,10 @@ var (
 		lines bool
 		endB bool
 		N int
+		cust []byte
 	} { semi: true, endB: true}
 	file string
+	esc_quot = true
 	silent bool
 	test_input = `#include <stdio.h>
 int main(void) {
@@ -21,6 +23,104 @@ int main(void) {
 `
 )
 
+func main() {
+	if file == "" && in == nil {
+		if !silent { fmt.Fprintln(os.Stderr, "reading from stdin...") }
+		scanr := bufio.NewScanner(os.Stdin)
+		for scanr.Scan() { in = append(in, scanr.Bytes()...) }
+		if e := scanr.Err(); e != nil { err_out("err reading stdin: %v", e) }
+	}
+
+	esc := struct {
+		on bool
+		typ byte
+	}{}
+
+	for _, b := range in {
+		switch b {
+		 case '\n': if !esc.on && count.lines { count.N++ }
+		 case ';': if !esc.on && count.semi { count.N++ }
+		 case '}': if !esc.on && count.endB { count.N++ } 
+		 case '"', '\'', '`':
+			if esc_quot {
+				if esc.typ == b {
+					esc.on = false ; esc.typ = 0
+				} else if !esc.on {
+					esc.on = true ; esc.typ = b
+				}
+				continue
+			}; fallthrough //handle parsing quoted strings (if enabled)
+		 default:
+			if byte_contains(count.cust, b) && !esc.on { count.N++ }
+		}
+	}
+	fmt.Printf("%d\n", count.N)
+}
+
+//I refuse to import slices and string
+//  (manual string parsing is fun)
+func contains_int(t int, s []int) bool {
+	for _, i := range s {
+		if i == t { return true }
+	}
+	return false
+}
+
+//I refuse to import slices and string
+//  (manual string parsing is fun)
+func idx_of_int(t int, s []int) int {
+	for idx, i := range s {
+		if i == t { return idx }
+	}
+	return -1
+}
+
+//I refuse to import slices and string
+//  (manual string parsing is fun)
+func split_str(s string) []byte {
+	var res []byte
+	for i, _ := range s { res = append(res, s[i]) }
+	fmt.Printf("\n%#v\n", res)
+	return res
+}
+
+//I refuse to import slices and string
+//  (manual string parsing is fun)
+func byte_contains(b_s []byte, b_I byte) bool {
+	for _, b := range b_s { if b == b_I { return true } }
+	return false
+}
+
+//priint to stderr and exit
+func err_out(s string, args ...any) {
+	fmt.Fprintf(os.Stderr, s+"\n", args...)
+	os.Exit(1)
+}
+
+//print help screen
+func print_help() {
+	lines := []string{
+		"'-f', '--file':",
+		"\tinput file",
+		"'--test':",
+		"\tuse embeded test file (good for demonstration)",
+		"'-s', '--silent':",
+		"\tsilent mode (no output)",
+		"'-l', '--lines':",
+		"\tinclude line numbers in count",
+		"'-B', '--no-brace', '--ignore-brace':",
+		"\tdon't count end braces",
+		"'-C', '--no-semi', '--ignore-semi-colons':",
+		"\tdon't count semi-colons", 
+		"'-h', '--help':",
+		"\tprints this",
+	}
+	for _, l := range lines { fmt.Println(l) }
+	os.Exit(0)
+}
+
+//I put this at the end because it's ugly
+//  it's just some crappy arg parsing
 func init() {
 	tak := []int{}
 	for i, a := range os.Args[1:] {
@@ -46,80 +146,17 @@ func init() {
 		 case "--ignore-brace", "--no-brace", "-B": count.endB = false
 		 case "--ignore-semi-colons", "--no-semi", "-C": count.semi = false
 		 case "-h", "--help": print_help()
+		 case "-q", "--parse-quotes": esc_quot = false
+		 case "-c", "--custom": 
+			if contains_int(i+1, tak) {
+				err_out("arg %s called but already taken (at %d)", a, idx_of_int(i+1, tak))
+			}
+			if len(os.Args[1:]) >= i+1 {
+				tak = append(tak, i+1)
+				count.cust = append(count.cust, split_str(os.Args[i+2])...)
+			} else { err_out("called %s but no value given", a) }
 		 default: err_out("invalid arg: %s", a)
 		}
 		tak = append(tak, i)
 	}
-}
-
-func main() {
-	esc := struct {
-		on bool
-		typ byte
-	}{}
-	if file == "" && in == nil {
-		if !silent { fmt.Fprintln(os.Stderr, "reading from stdin...") }
-		scanr := bufio.NewScanner(os.Stdin)
-		for scanr.Scan() { in = append(in, scanr.Bytes()...) }
-		if e := scanr.Err(); e != nil { err_out("err reading stdin: %v", e) }
-	}
-	for _, b := range in {
-		switch b {
-		 case '\n': if !esc.on && count.lines { count.N++ }
-		 case ';': if !esc.on && count.semi { count.N++ }
-		 case '"', '\'': if esc.typ == b {
-			esc.on = false ; esc.typ = 0
-			} else if !esc.on {
-				esc.on = true ; esc.typ = b
-			}
-		 case '}': if !esc.on && count.endB { count.N++ } 
-		}
-	}
-	fmt.Printf("%d\n", count.N)
-}
-
-//I refuse to import slices and string
-//  (manual string parsing is fun)
-func contains_int(t int, s []int) bool {
-	for _, i := range s {
-		if i == t { return true }
-	}
-	return false
-}
-
-//I refuse to import slices and string
-//  (manual string parsing is fun)
-func idx_of_int(t int, s []int) int {
-	for idx, i := range s {
-		if i == t { return idx }
-	}
-	return -1
-}
-
-func err_out(s string, args ...any) {
-	fmt.Printf(s+"\n", args...)
-	os.Exit(1)
-}
-
-func print_help() {
-	lines := []string{
-		"'-f', '--file':",
-		"\tinput file",
-		"'--test':",
-		"\tuse embeded test file (good for demonstration)",
-		"'-s', '--silent':",
-		"\tsilent mode (no output)",
-		"'-f', '--file'",
-		"\tinput file",
-		"'-l', '--lines':",
-		"\tinclude line numbers in count",
-		"'-B', '--no-brace', '--ignore-brace':",
-		"\tdon't count end braces",
-		"'-C', '--no-semi', '--ignore-semi-colons':",
-		"\tdon't count semi-colons", 
-		"'-h', '--help':",
-		"\tprints this",
-	}
-	for _, l := range lines { fmt.Println(l) }
-	os.Exit(0)
 }

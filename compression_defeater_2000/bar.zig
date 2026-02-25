3673d712d0ba5b5bf47be59db8808efdf671581e
keeper <97927259+Supraboy981322@users.noreply.github.com>
2026-02-25 09:09:34 -0600

    comments

--- compression_defeater_2000/bar.zig
+++ compression_defeater_2000/bar.zig
@@ -5,20 +5,28 @@ var stdout_wr = std.fs.File.stdout().writer(&stdout_buf);
 const stdout = &stdout_wr.interface;
 
 pub fn main() !void {
+    //some ascii chars to index into
     const chars:[]const u8 = "qwertzuiopasdfghjklyxcvbnm" 
             ++ ",./;'[]\\`1234567890-=+_~!@#$%^&*(){}|:\"><?";
+    //spawn a thread for listening to key presses
     const t = try std.Thread.spawn(.{}, keys, .{});
     t.detach();
+
+    //pseudo random 
     const r = std.crypto.random;
     while (true) {
+        //random char
         const c = chars[r.intRangeAtMost(usize, 0, chars.len-1)];
+        //random number for ansi color
         const n = r.intRangeAtMost(usize, 0, 6);
+        //print the char and the ansi sequence
         try stdout.print("\x1b[3{d}m{c}\x1b[0m", .{n, c});
         try stdout.flush();
     }
 }
 
 pub fn keys() !void { 
+    //get stdin (and the file discriptor)
     var buf:[1]u8 = undefined;
     var stdin_re = std.fs.File.stdin().reader(&buf);
     var stdin = &stdin_re.interface;
@@ -26,19 +34,20 @@ pub fn keys() !void {
 
     //prep terminal
     const termios = try std.posix.tcgetattr(fd);
-    const og_term_state = termios;
+    const og_term_state = termios; //save initial state
     var raw = og_term_state;
     raw.lflag.ICANON = false;
     raw.lflag.ECHO = false;
     try std.posix.tcsetattr(fd, .FLUSH, raw);
 
+    //infinitely listen to keypresses
     loop: while (true) {
         switch (try stdin.takeByte()) {
-            'q' => break :loop,
-            else => {},
+            'q' => break :loop, //break loop to cleanup when 'q' pressed 
+            else => {}, //ignore everything else
         }
-        buf = undefined;
     }
+    //reset term state and exit
     try std.posix.tcsetattr(fd, .FLUSH, og_term_state);
     std.process.exit(0);
 }

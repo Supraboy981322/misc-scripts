239022f341ec843c22197aeb0f9bb89d9add9424
keeper <97927259+Supraboy981322@users.noreply.github.com>
2026-03-19 09:17:09 -0500

    log remote ssh addr

--- seizure/main.go
+++ seizure/main.go
@@ -25,17 +25,20 @@ func init() {
 
 func main() {
 	ssh.Handle(func(s ssh.Session) {
-		log.Print("connection (ssh)")
+		log.Printf("connection (ssh): %s", s.RemoteAddr().String())
 		give_seizure(s)
 	})
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		log.Printf("request (http): %s", r.RemoteAddr)
 		give_seizure(w)
 	})
+
 	go func() {
 		log.Printf("ssh listening on port: %d", 22222)
 		panic(ssh.ListenAndServe(":22222", nil))
 	}()
+
 	go func() {
 		log.Printf("http listening on port: %s", port)
 		panic(http.ListenAndServe(":"+port, nil))
@@ -55,10 +58,10 @@ func random_hex() []byte {
 }
 
 func give_seizure(w io.Writer) { 
-		for {
-			frame := stolen_data
-			frame = bytes.ReplaceAll(frame, []byte("{{one}}"), random_hex())
-			frame = bytes.ReplaceAll(frame, []byte("{{two}}"), random_hex())
-			w.Write(frame)
-		}
+	for {
+		frame := stolen_data
+		frame = bytes.ReplaceAll(frame, []byte("{{one}}"), random_hex())
+		frame = bytes.ReplaceAll(frame, []byte("{{two}}"), random_hex())
+		w.Write(frame)
+	}
 }

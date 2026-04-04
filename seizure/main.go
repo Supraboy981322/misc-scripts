3f5e051a4a342d0eb885f9896b54341a15e1c377
keeper of the interwebs <97927259+Supraboy981322@users.noreply.github.com>
2026-04-04 13:54:10 -0500

    parse args for port

--- seizure/main.go
+++ seizure/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"os"
 	"io"
+	"fmt"
 	"log"
 	"net"
 	"bytes"
@@ -20,11 +21,25 @@ var stolen_data []byte
 //go:embed foo.html
 var browser_page []byte
 
-var port = "9983"
+type Protocol int
+const (
+	HTTP Protocol = iota
+	SSH
+	TCP
+)
+
+var ports = map[Protocol]int{
+	HTTP: 9983,
+	SSH: 4059,
+	TCP: 2085,
+}
 
 func init() {
 	if len(os.Args) > 1 {
-		port = os.Args[1]
+		for _, a := range os.Args[1:] {
+			parts := split(a)
+			ports[parts.proto] = parts.port
+		}
 	}
 	{
 		var css_stuff []byte
@@ -46,6 +61,21 @@ func init() {
 }
 
 func main() {
+	i := 3
+	fmt_port_print := func(which string, proto Protocol) string {
+		if len(which) == 3{ which = which + " " }
+		defer func(){i++}()
+		return fmt.Sprintf(
+			"\x1b[3%dm%s\x1b[38;2;100;100;150m {" +
+					" \x1b[0;38;2;255;165;0m%d" +
+					"\x1b[38;2;100;100;150m }\x1b[0m",
+			i,
+			which,
+			ports[proto],
+		)
+	}
+	log.Printf("\tlistening on ports:")
+
 	ssh.Handle(func(s ssh.Session) {
 		log.Printf("connection (ssh): %s", s.RemoteAddr().String())
 		give_seizure(s)
@@ -62,16 +92,19 @@ func main() {
 	})
 
 	go func() {
-		log.Printf("ssh listening on port: %d", 22222)
-		panic(ssh.ListenAndServe(":22222", nil))
+		log.Printf("\t\t%s", fmt_port_print("http", HTTP))
+		panic(http.ListenAndServe(":"+strconv.Itoa(ports[HTTP]), nil))
 	}()
 
 	go func() {
-		log.Printf("http listening on port: %s", port)
-		panic(http.ListenAndServe(":"+port, nil))
+		log.Printf("\t\t%s", fmt_port_print("ssh", SSH))
+		panic(ssh.ListenAndServe(":"+strconv.Itoa(ports[SSH]), nil))
 	}()
 
-	go panic(tcp())
+	go func() {
+		log.Printf("\t\t%s", fmt_port_print("TCP", TCP))
+		panic(tcp())
+	}()
 
 	select{}
 }
@@ -96,10 +129,9 @@ func give_seizure(w io.Writer) {
 }
 
 func tcp() error {
-	listener, e := net.Listen("tcp", ":7445")
+	listener, e := net.Listen("tcp", ":"+strconv.Itoa(ports[TCP]))
 	if e != nil { return e }
 	defer listener.Close() //shouldn't happen anyways
-	log.Printf("tcp listening on port 7445")
 
 	for {
 		conn, e := listener.Accept()
@@ -117,4 +149,67 @@ func is_browser(r *http.Request) bool {
 	if len(accept) < 1 { return false }
 	if len(bytes.Split([]byte(accept[0]), []byte{','})) > 1 { return true }
 	return false
+}
+
+func split(in string) struct{ proto Protocol; port int } {
+	for i, r := range in {
+		if r == '=' {
+
+			var proto_raw []rune
+			for _, r2 := range in[:i] {
+				if r2 >= 'A' && r2 <= 'Z' {
+					proto_raw = append(proto_raw, r2+32)
+				} else {
+					proto_raw = append(proto_raw, r2)
+				}
+			}
+
+			var proto Protocol
+			switch (string(proto_raw)) {
+				case "http": { proto = HTTP }
+				case "ssh":  { proto = SSH  }
+				case "tcp":  { proto = TCP  }
+				default: {
+					fmt.Fprintf(
+						os.Stderr,
+						"invalid or unsupported protocol: %s\n",
+						string(proto_raw),
+					)
+					os.Exit(1)
+				}
+			}
+
+			var port int
+			if i+1 >= len(in) { goto bad_port } else {
+				var e error
+				port, e = strconv.Atoi(in[i+1:])
+				if e != nil { goto bad_port }
+			}
+
+			return struct{
+				proto Protocol;
+				port int
+			} {
+				proto: proto,
+				port: port,
+			}
+
+			bad_port: {
+				fmt.Fprintf(
+					os.Stderr,
+					"invalid port number: %s\n",
+					in[i+1:],
+				)
+				os.Exit(1)
+			}
+		}
+	}
+
+	fmt.Fprintf(
+		os.Stderr,
+		"invalid port assignment (need something like this: 'http=9774'): %s",
+		in,
+	)
+	os.Exit(1)
+	panic(nil) 
 }

a519e578ed1fbed6e5b0070521e6a6655b96c1f9
keeper of the interwebs <97927259+Supraboy981322@users.noreply.github.com>
2026-04-08 12:16:55 -0500

    helpers to error on condition

--- print/src/helpers.zig
+++ print/src/helpers.zig
@@ -1,6 +1,8 @@
 const std = @import("std");
 const parser = @import("parser.zig");
 
+const stderr = &@constCast(&std.fs.File.stderr().writer(&.{})).interface;
+
 pub fn is_alpha(b:u8) bool {
     return for ([_]bool{
         b >= 'a' and b <= 'z',
@@ -21,4 +23,35 @@ pub fn str_is_num(raw:[]u8) bool {
     return for (str) |b| {
         if (b > '9' or b < '0') break false;
     } else true;
+}
+
+pub fn min_len(str:[]u8, len:usize) void {
+    if (str.len < len) {
+        stderr.print(
+            \\invalid escape:
+            \\  expected {d} valid characters, but found {d}
+            ++ "\n", .{len, str.len}
+        ) catch {};
+        std.process.exit(1);
+    }
+}
+
+pub fn err_if_not(condition:bool, comptime msg:[]const u8, fmt:anytype) void {
+    if (!condition) {
+        stderr.print(msg ++ "\n", fmt) catch {};
+        std.process.exit(1);
+    }
+}
+
+pub fn invalid_check(
+    condition:bool,
+    comptime what:[]const u8,
+    comptime additional:?[]const u8,
+    fmt:anytype
+) void {
+    err_if_not(
+        !condition,
+        "invalid " ++ what ++ "\n" ++ if (additional) |add| "\t" ++ add else "",
+        fmt
+    );
 }

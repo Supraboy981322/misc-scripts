6c61ddfb88611eb2f304578cbcab9d1d84b99cea
keeper <97927259+Supraboy981322@users.noreply.github.com>
2026-05-03 21:22:54 -0500

    update to Zig 0.16.0

--- music/src/main.zig
+++ music/src/main.zig
@@ -1,11 +1,11 @@
 const std = @import("std");
 
-const stdout = &@constCast(&std.fs.File.stdout().writer(&.{})).interface;
+pub fn main(init:std.process.Init) !void {
+    const alloc = init.gpa;
 
-pub fn main() !void {
-    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    defer _ = gpa.deinit();
-    const alloc = gpa.allocator();
+    var stdout_buf:[1024]u8 = undefined;
+    var stdout_wr = std.Io.File.stdout().writer(init.io, &stdout_buf);
+    const stdout = &stdout_wr.interface;
 
     var args = [8][]const u8 {
         "ffplay",
@@ -24,27 +24,34 @@ pub fn main() !void {
         for (args) |a|
             alloc.free(a);
 
-    var upnext = try pick_item(alloc);
+    var upnext = try pick_item(init.io, alloc);
     defer alloc.free(upnext);
     try stdout.print("\n\n\n", .{});
+    try stdout.flush();
     while (true) {
         alloc.free(args[7]);
         args[7] = try alloc.dupe(u8, upnext);
         alloc.free(upnext);
-        upnext = try pick_item(alloc);
+        upnext = try pick_item(init.io, alloc);
         try stdout.print(
             "\x1b[3A\x1b[2K\r\x1b[33mplaying: "
                 ++ "\x1b[34m{s}\x1b[0m\n\x1b[2K\t\x1b[35mupnext: "
                     ++ "\x1b[36m{s}\x1b[0m\n\x1b[2K",
             .{ args[7], upnext }
         );
-        var child = std.process.Child.init(&args, alloc);
-        _ = try child.spawnAndWait();
+        try stdout.flush();
+        var child = try std.process.spawn(init.io, .{
+            .argv = &args,
+            .stdout = .pipe,
+            .stderr = .pipe,
+            .stdin = .pipe,
+        });
+        _ = try child.wait(init.io);
     }
 }
 
-pub fn pick_item(alloc:std.mem.Allocator) ![]const u8 {
-    var dir = try std.fs.cwd().openDir(".", .{ .iterate = true });
+pub fn pick_item(io:std.Io, alloc:std.mem.Allocator) ![]const u8 {
+    var dir = try std.Io.Dir.cwd().openDir(io, ".", .{ .iterate = true });
     var walker = try dir.walk(alloc);
     defer walker.deinit();
     var arr = try std.ArrayList([]const u8).initCapacity(alloc, 0);
@@ -53,10 +60,11 @@ pub fn pick_item(alloc:std.mem.Allocator) ![]const u8 {
             alloc.free(file);
         arr.deinit(alloc);
     }
-    while (try walker.next()) |entry| if (entry.kind == .file) {
+    while (try walker.next(io)) |entry| if (entry.kind == .file) {
         try arr.append(alloc, try alloc.dupe(u8, entry.basename));
     };
-    const idx = std.crypto.random.uintAtMost(usize, arr.items.len - 1);
+    const random = (std.Random.IoSource{ .io = io }).interface();
+    const idx = random.uintAtMost(usize, arr.items.len - 1);
     const picked = arr.items[idx];
     return alloc.dupe(u8, picked);
 }

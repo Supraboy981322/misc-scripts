083b8c8d0efa99586708f32756aa81c6cbcf0e28
keeper <97927259+Supraboy981322@users.noreply.github.com>
2026-05-24 14:33:02 -0500

    cleanup ; some comments

--- yes_clone/foo.asm
+++ yes_clone/foo.asm
@@ -1,8 +1,8 @@
 section .data
-  argc dd 0 ;4 bytes wide
+  argc dd 0 ; NOTE: 4 bytes wide
+
   not_enough_args db "not enough args", 10
   not_enough_args_len equ $ - not_enough_args
-  exit_code db 0
 
 section .text
   global _start
@@ -10,37 +10,36 @@ section .text
 _start:
 
   mov rdi, [rsp] ;argc
-  cmp rdi, 2
+  cmp rdi, 2     ;if argc -lt 2 then need_args
   jl need_args
 
-  mov rsi, [rsp + 16]
-  mov rdi, 0
+  mov rsi, [rsp + 16] ;argv[1]
+  mov rdx, 0          ;length of argv[1]
   count:
-    movzx eax, byte [rsi + rdi]
-    test al, al
-    jz counted
-    inc rdi
-    jmp count
+    movzx eax, byte [rsi + rdx] ;get byte at idx rdi
+    test al, al                 ;set zero flag if byte is 0 (NUL)
+    jz counted                  ;end loop on zero (NUL) byte
+    inc rdx                     ;increment counter
+    jmp count                   ;continue loop
   counted:
-  mov rdx, rdi
 
-  mov rsi, [rsp + 16]
-  mov byte [rsi + rdx], 10
-  add rdx, 1
+  mov byte [rsi + rdx], 10 ;replace NUL byte with newline
+  inc rdx                  ;counter (len of argv[1]) to include newline
 
   continue:
     mov rax, 1 ;write
     mov rdi, 1 ;stdout
     syscall
-  jmp continue
+  jmp continue ;loop indefinitely
 
-  mov rdi, 0
-  jmp exit
+  mov rdi, 0 ;set exit code
+  jmp exit   ;jmp to exit
 
 
+;prints "not enough args" (followed by newline) then exits with code '1'
 need_args:
   mov rax, 1
-  mov rdi, 2
+  mov rdi, 2 ;stderr
   mov rsi, not_enough_args
   mov rdx, not_enough_args_len
   syscall
@@ -50,5 +49,5 @@ need_args:
 
 
 exit:
-  mov rax, 60
+  mov rax, 60 ;exit
   syscall

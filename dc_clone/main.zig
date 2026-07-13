e5787e50ac6bd2c43f33a395036c371bc70cee63
keeper <97927259+Supraboy981322@users.noreply.github.com>
2026-07-12 23:47:22 -0500

    start rewrite with std.math.bit.int.Managed

--- dc_clone/main.zig
+++ dc_clone/main.zig
@@ -1,193 +1,139 @@
-const write = @import("std").posix.system.write;
-const read = @import("std").posix.system.read;
-const errno = @import("std").posix.system.errno;
-const fd_t = @import("std").posix.system.fd_t;
+const std = @import("std");
 
-// TODO: refactor for f34
+const isDigit = std.ascii.isDigit;
+const isWhitespace = std.ascii.isWhitespace;
+const isHex = std.ascii.isHex;
+const exit = std.process.exit;
+const int = std.math.big.int.Managed;
 
-const Cmd = enum(u8) {
-    exit,
-    print,
-    sqrt,
-    add, sub,
-    div, mult,
-    discard,
+var io:std.Io = undefined;
 
-    // TODO:
-    pow,
-    root,
-};
+var stack:[256]int = undefined;
+var stack_top:[*]int = stack[0..].ptr;
+var pos:usize = 0;
 
-var stack = [_]i32{0} ** 256;
-var top:[*]i32 = undefined;
-inline fn push(s:i32) void {
-    top[0] = s;
-    top += 1;
+var registers = [_]int{undefined} ** 16;
+
+fn push(num:int) void {
+    pos += 1;
+    if (pos > stack.len) @panic("stack overflow");
+    stack_top[0] = num;
+    stack_top += 1;
 }
-inline fn pop() i32 {
-    top -= 1;
-    return top[0];
+fn pop() ?int {
+    if (pos == 0) return null;
+    pos -= 1;
+    stack_top -= 1;
+    return stack_top[0];
 }
-
-inline fn print(msg:[]const u8) void {
-    fprint(1, msg);
-}
-fn fprint(fd:fd_t, msg:[]const u8) void {
-    const ret = write(fd, msg.ptr, msg.len);
-    if (errno(ret) != .SUCCESS) @panic("failed to write to stdout");
-}
-
-pub fn main() u8 {
-
-    top = (&stack).ptr;
-    var buf:[1024]u8 = undefined;
-
-    while (true) {
-        defer print("\n");
-        print("\n\x1b[33m(?):\x1b[0m ");
-
-        const n = read(0, &buf, 1024);
-        var line = buf[0..n];
-
-        while (shift(&line)) |word| {
-            if (word.len == 0) continue;
-            if (is_num(word))
-                push(parse_num(word))
-            else if (mk_cmd(word)) |cmd| {
-                if (do_cmd(cmd)) return 0;
-            } else {
-                inline for ([_][]const u8{
-                    "invalid command: |", word, "|\n"
-                }) |seg|
-                    fprint(2, seg);
-            }
-        }
+fn getReg(reader:*std.Io.Reader) !*int {
+    var buf:[2]u8 = .{0, 0};
+    for (&buf) |*b| {
+        b.* = try reader.takeByte();
     }
+    if (buf[0] != 'r' or !isHex(buf[1]))
+        return error.InvalidRegister;
+    const num = switch (buf[1]) {
+        '0'...'9' => |n| n-'0',
+        'a'...'f' => |n| n-'a'+10,
+        else => unreachable,
+    };
+    return @ptrCast(registers[num..].ptr);
+}
+
+pub fn main(init:std.process.Init) !u8 {
+    const alloc = init.gpa;
+    io = init.io;
+
+    for (&registers) |*reg| reg.* = try .init(alloc);
+    for (&stack) |*i| i.* = try .init(alloc);
+    defer {
+        for (&registers) |*reg| reg.deinit();
+        for (&stack) |*i| i.deinit();
+    }
+
+    var in_buf:[1024]u8 = undefined;
+    var stdin = std.Io.File.stdin().reader(init.io, &in_buf);
+    const reader = &stdin.interface;
+
+    while (reader.takeByte()) |b| {
+        if (isWhitespace(b)) continue;
+
+        switch (b) {
+            'q' => exit(0),
+
+            'p' => {
+                if (pos == 0) {
+                    try print(.err, "stack empty");
+                    continue;
+                }
+                var num = (stack_top-1)[0];
+                const str = try num.toString(alloc, 10, .lower);
+                defer alloc.free(str);
+                try print(.out, str);
+            },
+
+            's' => {
+                var reg = try getReg(reader);
+                var num = pop() orelse {
+                    try print(.err, "stack empty");
+                    continue;
+                };
+                defer num.deinit();
+                reg.swap(&num);
+            },
+
+            '+' => {
+                var one = pop() orelse {
+                    try print(.err, "stack empty");
+                    continue;
+                };
+                defer one.deinit();
+                var two = pop() orelse {
+                    try print(.err, "stack empty");
+                    continue;
+                };
+                defer two.deinit();
+                var new:int = try .init(alloc);
+                try new.add(&one, &two);
+                push(new);
+            },
+
+            't' => {
+                var n = pop() orelse {
+                    try print(.err, "stack empty");
+                    continue;
+                };
+                n.deinit();
+            },
+
+            '0'...'9' => |n| {
+                var buf:std.ArrayList(u8) = .empty;
+                defer buf.deinit(alloc);
+                try buf.append(alloc, n);
+                while (isDigit(reader.peekByte() catch 0))
+                    try buf.append(alloc, try reader.takeByte());
+                var num:int = try .initSet(alloc, 0);
+                try num.setString(10, buf.items);
+                push(num);
+            },
+
+            else => {
+                try print(.err, "unknown op");
+                break;
+            },
+        }
+    } else |err| return err;
+
     return 0;
 }
 
-fn do_cmd(cmd:Cmd) bool {
-    switch (cmd) {
-        inline .add, .div, .sub, .mult => |w| {
-            const two = pop();
-            const one = pop();
-            const v = switch (comptime w) {
-                .add => one + two,
-                .sub => one - two,
-                .div => @divTrunc(one, two),
-                .mult => one * two,
-                else => unreachable,
-            };
-            push(v);
-        },
-        .sqrt => push(sqrt(pop())),
-        .discard => _ = pop(),
-        .print => {
-            var buf:[10]u8 = undefined;
-            var b:[]u8 = &buf;
-            to_str(&b, (top - 1)[0]);
-            print(b);
-        },
-        .exit => return true,
-        else => unreachable,
-    }
-    return false;
-}
-
-fn parse_num(str:[]u8) i32 {
-    var res:i32 = 0;
-    const s = if (str[0] == '-') str[1..] else str;
-    for (s) |b| {
-        res *= 10;
-        res += b - '0';
-    }
-    if (str[0] == '-') res *= -1;
-    return res;
-}
-
-inline fn sqrt(v:i32) i32 {
-    return @intFromFloat(@sqrt(@as(f32, @floatFromInt(v))));
-}
-fn dig(v:u8) [2]u8 {
-    return .{ @intCast('0' + v / 10), @intCast('0' + v % 10) };
-}
-fn to_str(buf:*[]u8, val:i32) void {
-    const abs = @abs(val);
-    var a = abs;
-    var i:usize = buf.len;
-    while (a >= 100) : (a = @divTrunc(a, 100)) {
-        i -= 1;
-        buf.*[i..][0..2].* = dig(@intCast(a % 100));
-    }
-    i -= 1;
-    if (a < 10)
-        buf.*[i] = '0' + @as(u8, @intCast(a))
-    else {
-        i -= 1;
-        buf.*[i..][0..2].* = dig(@intCast(a));
-    }
-
-    i -= 1;
-    if (val < 0) {
-        buf.*[i] = '-';
-    } else
-        buf.* = buf.*[1..buf.len];
-    buf.* = buf.*[i..];
-}
-
-inline fn is_space(b:u8) bool {
-    return b == ' ' or (b >= '\t' and b <= '\r');
-}
-fn is_num(str:[]u8) bool {
-    const s = if (str[0] == '-' and str.len > 1) str[1..] else str;
-    for (s) |b| if (b < '0' or b > '9') return false;
-    return true;
-}
-fn mk_cmd(str:[]u8) ?Cmd {
-    if (str_eql_any(str, &.{ "+", "add" })) return .add;
-    if (str_eql_any(str, &.{ "-", "sub" })) return .sub;
-    if (str_eql_any(str, &.{ "*", "mult" })) return .mult;
-    if (str_eql_any(str, &.{ "/", "div" })) return .div;
-    if (str_eql_any(str, &.{ "v", "sqrt" })) return .sqrt;
-    if (str_eql_any(str, &.{ "d", "discard" })) return .discard;
-    if (str_eql_any(str, &.{ "p", "print" })) return .print;
-    if (str_eql_any(str, &.{ "q", "exit", "quit" })) return .exit;
-    return null;
-}
-fn str_eql(one:[]const u8, two:[]const u8) bool {
-    if (one.len != two.len) return false;
-    for (0..one.len) |i| if (two[i] != one[i]) return false;
-    return true;
-}
-fn str_eql_any(str:[]const u8, comptime checks:[]const []const u8) bool {
-    return inline for (checks) |chk| {
-        if (str_eql(str, chk)) break true;
-    } else
-        false;
-}
-
-
-fn trim(str:*[]u8) void {
-    var start:usize = 0;
-    while (is_space(str.*[start])) : (start += 1) {}
-    var end:usize = str.len-1;
-    while (is_space(str.*[end])) : (end -= 1) {}
-    str.* = str.*[start..end];
-}
-
-fn shift(string:*[]u8) ?[]u8 {
-    var i:usize = 0;
-    while (i < string.len) : (i += 1) {
-        if (is_space(string.*[i])) {
-            const word = string.*[0..i];
-            while (i < string.len and is_space(string.*[i])) : (i += 1) {}
-            string.* = string.*[i..];
-            return word;
-        }
-    }
-    if (string.len > 0) {
-        defer string.* = string.*[0..0];
-        return string.*;
-    }
-    return null;
+fn print(where:enum{out, err}, str:[]const u8) !void {
+    var out_buf:[1024]u8 = undefined;
+    var wr = switch (where) {
+        .out => std.Io.File.stdout().writer(io, &out_buf),
+        .err => std.Io.File.stderr().writer(io, &out_buf),
+    };
+    try wr.interface.print("{s}\n", .{str});
+    try wr.interface.flush();
 }

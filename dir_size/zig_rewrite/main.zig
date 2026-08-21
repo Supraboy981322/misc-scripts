7e6555c3aa4ad696f490ddbc603ac1a8063fef1b
keeper <97927259+Supraboy981322@users.noreply.github.com>
2026-08-21 11:05:16 -0500

    parse args ; multiple starting dirs ; placeholders

--- dir_size/zig_rewrite/main.zig
+++ dir_size/zig_rewrite/main.zig
@@ -2,12 +2,28 @@ const std = @import("std");
 
 var stuff:std.process.Init = undefined;
 var counter:std.atomic.Value(usize) = .init(0);
+var starting_dirs:std.ArrayList([]const u8) = .empty; //TODO
+var verbose:bool = false;
+var pattern:[]const u8 = "*";
+
+const eql = std.mem.eql;
 
 pub fn main(init:std.process.Init) !u8 {
     stuff = init;
-    const cwd = try std.Io.Dir.cwd().openDir(stuff.io, ".", .{ .iterate = true });
-    try recurse(cwd);
-    std.log.info("total: {d}", .{counter.load(.seq_cst)});
+    defer starting_dirs.deinit(init.gpa);
+    doArgs() catch |err| {
+        std.log.err("failed to parse arguments: {t}", .{err});
+        return 1;
+    };
+    defer std.log.info("counted: {d}", .{counter.load(.seq_cst)});
+    if (starting_dirs.items.len == 0) try starting_dirs.append(stuff.gpa, ".");
+    var wg:std.Io.Group = .init;
+    defer wg.cancel(stuff.io);
+    for (starting_dirs.items) |dirname| {
+        const dir = try std.Io.Dir.cwd().openDir(stuff.io, dirname, .{ .iterate = true });
+        wg.async(stuff.io, recurseShim, .{dir});
+    }
+    try wg.await(stuff.io);
     return 0;
 }
 
@@ -38,4 +54,58 @@ pub fn recurse(dir:std.Io.Dir) !void {
         },
     };
     try wg.await(stuff.io);
+}
+
+pub fn doArgs() !void {
+    var itr = stuff.minimal.args.iterate();
+    _ = itr.skip();
+    while (itr.next()) |arg| {
+        errdefer std.log.info("here -> |{s}|", .{arg});
+        if (arg.len > 0) if (arg[0] == '-') {
+            if (arg[1] == '-')
+                try flagArg(&itr, arg[2..])
+            else
+                try bundleArg(&itr, arg[1..]);
+            continue;
+        };
+        try starting_dirs.append(stuff.gpa, arg);
+    }
+}
+
+pub fn flagArg(itr:*std.process.Args.Iterator, flag:[]const u8) !void {
+    if (flag.len == 0) {
+        while (itr.next()) |arg| try starting_dirs.append(stuff.gpa, arg);
+        return;
+    }
+    if (eql(u8, flag, "verbose")) {
+        verbose = true;
+        return;
+    }
+    if (eql(u8, flag, "pattern")) {
+        pattern = itr.next() orelse {
+            return error.MissingArgumentValue;
+        };
+        return;
+    }
+    return error.UnknownArgument;
+}
+
+pub fn bundleArg(itr:*std.process.Args.Iterator, bundle:[]const u8) !void {
+    if (bundle.len == 0) {
+        std.log.warn("reading list of directories from stdin", .{});
+        var buf:[std.posix.PATH_MAX]u8 = undefined;
+        var stdin = std.Io.File.stdin().reader(stuff.io, &buf);
+        while (try stdin.interface.takeDelimiter('\n')) |dir| {
+            try starting_dirs.append(stuff.gpa, dir);
+        }
+        return;
+    }
+    for (bundle) |b| switch (b) {
+        'V' => verbose = true,
+        'p' => pattern = itr.next() orelse return error.MissingArgumentValue,
+        else => {
+            std.log.info("this -> |{c}|", .{b});
+            return error.UnknownArgument;
+        }
+    };
 }
